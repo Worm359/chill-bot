445f47e000344c17d45fadc65a88c773ee577e5d
ialbert <ialbert@mfd.ru>
2023-07-17 21:04:13 +0300

    clean and launch

---
+++ src/main/java/ru/worm/discord/chill/ChillBotApplication.java
@@ -0,0 +1,16 @@
+package ru.worm.discord.chill;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChillBotApplication {
+    private static final Logger log = LoggerFactory.getLogger(ChillBotApplication.class);
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(ChillBotApplication.class);
+        app.run(args);
+        log.info("Hello world!");
+    }
+}
