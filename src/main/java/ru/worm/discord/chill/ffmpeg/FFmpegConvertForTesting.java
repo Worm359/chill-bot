e515a035cc9a45fce98a949f62823184feabe48e
ialbert <ialbert@mfd.ru>
2023-08-01 22:33:23 +0300

    FFmpegConvertForTesting более точная команда для создания тестовых opus файлов

--- src/main/java/ru/worm/discord/chill/ffmpeg/FFmpegConvertForTesting.java
+++ src/main/java/ru/worm/discord/chill/ffmpeg/FFmpegConvertForTesting.java
@@ -15,7 +15,8 @@ public class FFmpegConvertForTesting {
         //ProcessBuilder pb = new ProcessBuilder(ffmpeg, "-i", "/path/to/input.mp4", "-vcodec", "h264", "/path/to/output.mp4");
         //пример для конвертации в формат opus
         //-i input.mp3 -c:a libopus output.opus
-        ProcessBuilder pb = new ProcessBuilder(ffmpeg, "-i", "pupushka.mp3", "-c:a", "libopus", "output.opus");
+        //ProcessBuilder pb = new ProcessBuilder(ffmpeg, "-i", "pupushka.mp3", "-c:a", "libopus", "output.opus");
+        ProcessBuilder pb = new ProcessBuilder(ffmpeg, "-i", "pupushka.mp3", "-c:a", "libopus", "-b:48000", "-frame_duration 20", "output2.opus");
         pb.inheritIO().start().waitFor();
     }
 }
