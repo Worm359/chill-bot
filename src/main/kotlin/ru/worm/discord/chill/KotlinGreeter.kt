218cfc45dc955d8d280d8424f0a521ed9ae6ce65
worm359 <worm359@gmail.com>
2024-04-28 23:57:07 +0500

    kotlin init log

--- src/main/kotlin/ru/worm/discord/chill/KotlinGreeter.kt
+++ src/main/kotlin/ru/worm/discord/chill/KotlinGreeter.kt
@@ -1,7 +1,11 @@
 package ru.worm.discord.chill
 
+import org.slf4j.LoggerFactory
+
 class KotlinGreeter {
-    fun greet():Unit {
-        println("kotlin is up and running.")
+    val log = LoggerFactory.getLogger(KotlinGreeter::class.java)
+
+    fun greet() {
+        log.info("kotlin is up and running.")
     }
 }
